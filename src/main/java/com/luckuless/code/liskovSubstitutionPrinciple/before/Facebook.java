e8af1b58269580e3c59b578771bd96749c900f88
luckuless <luckuless.rambojun@icloud.com>
2023-05-11 11:55:51 +0200

    LSP: Social Media Implementation with Facebook

---
+++ src/main/java/com/luckuless/code/liskovSubstitutionPrinciple/before/Facebook.java
@@ -0,0 +1,25 @@
+package com.luckuless.code.liskovSubstitutionPrinciple.before;
+
+public class Facebook extends SocialMedia {
+
+    @Override
+    public void chatWithFriend() {
+        // logic
+    }
+
+    @Override
+    public void publishPost(Object post) {
+        // logic
+    }
+
+    @Override
+    public void sendPhotosAndVideos() {
+       // logic
+    }
+
+    @Override
+    public void groupVideoCall(String... users) {
+       // logic
+    }
+    
+}
