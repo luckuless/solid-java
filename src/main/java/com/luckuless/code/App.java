01e7130846a37b8710d85564e639658b34b6d888
luckuless <luckuless.rambojun@icloud.com>
2023-05-11 10:52:35 +0200

    repo setup

---
+++ src/main/java/com/luckuless/code/App.java
@@ -0,0 +1,13 @@
+package com.luckuless.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
