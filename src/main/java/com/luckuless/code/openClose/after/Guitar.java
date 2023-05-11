e25ab3b7bef5424843fb51254e2a3d35d5b1a767
luckuless <luckuless.rambojun@icloud.com>
2023-05-11 11:36:23 +0200

    open-close: fix-GuitarWithFlamePattern extends Guitar

---
+++ src/main/java/com/luckuless/code/openClose/after/Guitar.java
@@ -0,0 +1,11 @@
+package com.luckuless.code.openClose.after;
+
+// open for extension, closed for modification
+public class Guitar {
+    String name;
+    String make;
+    String volume;
+
+    // Constructors setters and getters
+    
+}
---
+++ src/main/java/com/luckuless/code/openClose/after/GuitarWithFlamePattern.java
@@ -0,0 +1,8 @@
+package com.luckuless.code.openClose.after;
+
+// extends Guitar with flame colour attribute
+public class GuitarWithFlamePattern extends Guitar {
+
+    String flameColour;
+    
+}
